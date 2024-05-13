11381e97afd8a733e9489a106ad115b5a48ec3a8
Dreed115 <155508461+Dreed115@users.noreply.github.com>
2024-05-13 11:50:06 -0600

    Add files via upload

---
+++ Sign Recognition/Image_Collector.py
@@ -0,0 +1,43 @@
+import os
+import cv2
+
+dir = ".\Img"
+
+if not os.path.exists(dir):
+    os.makedirs(dir)
+
+number_of_cases = 26
+dataset_size = 100
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Not opened")
+
+for j in range(number_of_cases):
+    if not os.path.exists(os.path.join(dir, str(j))):
+        os.makedirs(os.path.join(dir, str(j)))
+
+    print("Collecting data for class {}".format(j))
+
+    done = False
+
+    while True:
+        ret, frame, = cap.read()
+    
+        cv2.putText(frame, "Ready? Press Q!: ", (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+        cv2.imshow("frame", frame)
+        if cv2.waitKey(25) == ord("q"):
+            break
+
+    counter = 0
+    while counter < dataset_size:
+        ret, frame = cap.read()
+        cv2.imshow("frame", frame)
+        cv2.waitKey(25)
+        cv2.imwrite(os.path.join(dir, str(j), "{}.jpg".format(counter)), frame)
+
+        counter += 1
+
+cap.release()
+cv2.destroyAllWindows()
+
